062072bb4bb758f0a1b81cf67c4cf4bd8f15b820
Ipwik <ipwikum@gmail.com>
2023-04-30 12:46:52 +0300

    Server now accepts 2 forms: register the user (check for valid name) and handle incoming message.

--- server/main.go
+++ server/main.go
@@ -2,19 +2,59 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
-func main() {
-	http.HandleFunc("/", handleMessage)
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("server error: ", err)
-	}
+type info struct {
+	users    []string
+	messages []message
 }
 
-func handleMessage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, Bob here from bob vance refrigerators")
+type message struct {
+	username    string
+	message     string
+	messageTime time.Time
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	users := make(map[string]struct{})
+
+	mux.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal("Server/register: Could not read body: ", err)
+		}
+		name := string(body)
+
+		if _, ok := users[name]; ok {
+			io.WriteString(w, "Name already exists")
+		} else if len(name) < 2 {
+			io.WriteString(w, "Name too short")
+		} else if len(name) > 10 {
+			io.WriteString(w, "name too long")
+		} else {
+			users[name] = struct{}{}
+			io.WriteString(w, "Registered! Hello ")
+			io.WriteString(w, name)
+		}
+	})
+	mux.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal("Server/message: Could not read body: ", err)
+		}
+		message := string(body)
+
+		io.WriteString(w, message)
+	})
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal("Server: error: ", err)
+	}
 }
